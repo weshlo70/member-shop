66ea54fbb0a4b7e3e7d3bba66ac4836088b794f1
weshlo70 <168162523+weshlo70@users.noreply.github.com>
2026-09-07 14:13:19 +0800

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -11,7 +11,7 @@ let checkoutTotal = 0;
 // ========================================
 
 const CHECKOUT_API_URL =
-    "你的 Apps Script 網址";
+    "https://script.google.com/macros/s/AKfycbzoiQXbZQt12Ez_2yz2aQo4guCJwrmggYgGZr2XhB1N0xXXXH7qLUmp4-9xzDhMCvtN/exec";
 
 
 // ========================================
